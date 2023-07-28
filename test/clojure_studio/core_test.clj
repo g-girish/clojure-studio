d23eb39115d1ab39326c484d7f8f8b6da69cb712
Girish <g.girish@globallogic.com>
2023-07-28 11:28:21 +0530

    feat: test case for menu

--- test/clojure_studio/core_test.clj
+++ test/clojure_studio/core_test.clj
@@ -1,7 +1,13 @@
 (ns clojure-studio.core-test
   (:require [clojure.test :refer :all]
-            [clojure-studio.core :refer :all]))
+            [clojure-studio.core :refer :all]
+            [resource-mgr.restaurant :refer [getMenu]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest get-items-from-menu
+  (testing "test getMenu function")
+  (let [items (getMenu)]
+    (is (not-empty items))))
